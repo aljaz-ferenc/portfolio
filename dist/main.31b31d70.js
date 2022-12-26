9aca3186cd74a73177723b7b627119c4a046c705
aljaz-ferenc <aljaz.ferenc@protonmail.com>
2022-12-26 18:34:10 +0100

    add react icon

--- dist/main.31b31d70.js
+++ dist/main.31b31d70.js
@@ -197,7 +197,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50216" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50389" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
